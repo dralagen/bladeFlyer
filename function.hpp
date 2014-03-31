2f703a1d3104859cd4bac03e1e798bbed35b7637
Kelite <hippo.gandon@live.fr>
2014-03-31 15:28:26 +0200

    Update function.hpp

--- function.hpp
+++ function.hpp
@@ -4,8 +4,8 @@ std::list<std::list<int> > enumCli(int* demCli, int nbCli, int cap);
 
 void enumCli_aux(std::list<std::list<int> > &l, int* demCli, int nbCli, int totalCap, int cli, int cap, std::list<int> lastList);
 
-list<list<int>> permutdouble(list<list<int>> ec);
-void permutbourrin(list<list<int>> res, list<int> new, list<list<int>> old);
+std::list<std::list<int>> permutdouble(std::list<std::list<int>> ec);
+void permutbourrin(std::list<std::list<int>> res, std::list<int> new, std::list<std::list<int>> old);
 
 void print_list(const std::list<int> &l);
 void print_dlist(const std::list< std::list<int> > &l);
