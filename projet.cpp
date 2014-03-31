44cc82e248a751b82274579bdd1407d9df36ec4c
Kelite <hippo.gandon@live.fr>
2014-03-31 17:54:01 +0200

    Update projet.cpp

--- projet.cpp
+++ projet.cpp
@@ -140,7 +140,7 @@ int main(int argc, char *argv[])
 	for (i = 1; i < 5; i++){
 		l.push_back(i);
 	}
-	cout << long(l) << endl;
+	cout << long(l, p.C) << endl;
 	
 	/* Problème résolu, arrêt du chrono */
 
