7f6f558f090e326088c0eefe0b59cae50f09015a
Kelite <hippo.gandon@live.fr>
2014-04-01 10:25:23 +0200

    Update function.hpp

--- function.hpp
+++ function.hpp
@@ -9,7 +9,7 @@ std::list<std::list<int> > enumCli(int* demCli, int nbCli, int cap);
 
 void enumCli_aux(std::list<std::list<int> > &l, int* demCli, int nbCli, int totalCap, int cli, int cap, std::list<int> lastList);
 
-regroupement permutdouble(std::list<std::list<int>> ec, int** mat);
+std::list<regroupement> permutdouble(std::list<std::list<int>> ec, int** mat);
 void permutbourrin(regroupement &res, std::list<int> nouv, std::list<int> old, int** mat);
 
 int CalLong(std::list<int> &l, int** mat);
